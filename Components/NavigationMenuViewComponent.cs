8c60409264562c370939f97a370d49fd695f05bd
zjf1018 <jingfengzheng@icloud.com>
2022-09-01 23:16:31 -0400

    add Component

---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+        }
+    }
+}
+
